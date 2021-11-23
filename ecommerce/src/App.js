e6664993982e56a526320bceafc6623f3db9cb90
shainasm <smulani2002@google.com>
2021-11-23 20:45:18 +0530

    footer

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,14 +5,15 @@ import Signup from "./components/Signup";
 import Products from "./components/Products";
 import Order from "./components/Order";
 import MyOrders from "./components/MyOrders";
-import Navbar from "./layout/Navbar";
+import Navbar from "./layout/Navbar"
+import Footer from "./layout/Footer";
 import { Routes, Route } from 'react-router-dom'
 import PrivateRoute from "./routing/PrivateRoute";
 
 const App = () => {
   return ( 
     <div>
-    <Navbar />
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login/>} />
@@ -20,6 +21,8 @@ const App = () => {
       <Route path='/products' element={<Products />} />
       <Route path='/admin' element={<AdminPage />} />
     </Routes>
+    <Footer />
+
   </div>
   );
 }
